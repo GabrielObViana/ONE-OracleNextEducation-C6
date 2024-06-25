0192a773a1a6d05596b4b9891a574af7ace0d1f7
Gabriel Viana <gabriel.ob.viana@gmail.com>
2024-06-25 19:11:05 -0300

    Add new project 7.CarPrices-with-Spring

---
+++ 7.CarPricesProject-with-Spring/src/main/java/br/com/oracle/carprices/model/Modelos.java
@@ -0,0 +1,9 @@
+package br.com.oracle.carprices.model;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+
+import java.util.List;
+
+@JsonIgnoreProperties(ignoreUnknown = true)
+public record Modelos(List<Dados> modelos) {
+}
---
+++ 7.CarPricesProject-with-Spring/src/test/java/br/com/oracle/carprices/TabelafipeApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.oracle.carprices;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TabelafipeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
